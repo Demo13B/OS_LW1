d66c4e5bda985c98615b171438b38f71c31f5612
TRSalikhov <trsalikhov@mai.education>
2023-09-19 23:02:00 +0300

    Removed cin, now using read()

--- main.cpp
+++ main.cpp
@@ -1,44 +1,62 @@
-#include <iostream>
 #include <unistd.h>
+#include <iostream>
 
-auto main() -> int
-{
-    int fd[2]; // fd[0] - read; fd[1] - write
+auto main() -> int {
+    int fd[2];  // fd[0] - read; fd[1] - write
     // Creating pipe and checking that it was created
-    if (pipe(fd) == -1)
-    {
+    if (pipe(fd) == -1) {
         std::cerr << "Pipe creation failed\n";
         return 1;
     }
 
     // Forking the process
     int id = fork();
-    if (id == -1) // fork error
-    {
+
+    if (id == -1) {  // fork error
         std::cerr << "Could not fork a process";
         return 2;
-    }
-    else if (id == 0) // child process
-    {
+    } else if (id == 0) {  // child process
         close(fd[1]);
         dup2(fd[0], STDIN_FILENO);
-        execlp("./calculator", "./calculator", NULL);
-        std::cerr << "Failed to excute child";
-        return 3;
-    }
-    else // parent process
-    {
-        close(fd[0]);
-        std::string number;
 
-        while (std::cin >> number)
-        {
-            if (write(fd[1], &number, sizeof(number)) == -1)
-            {
-                std::cerr << "Could not write into pipe\n";
+        execlp("./calculator", "./calculator", NULL);
+
+        std::cerr << "Failed to excute child\n";
+        return 3;
+    } else {  // parent process
+        close(fd[0]);
+        std::string number = "";
+        char c;
+
+        if (read(STDIN_FILENO, &c, sizeof(char)) == -1) {
+            std::cerr << "Could not read from stdin\n";
+            return 4;
+        }
+
+        while (c != '\n') {  // while no newline character
+            if (c == ' ') {  // number finished
+                if (write(fd[1], &number, sizeof(number)) == -1) {
+                    std::cerr << "Could not write into pipe\n";
+                    return 5;
+                }
+                number = "";
+            } else {  // new digit
+                number += c;
+            }
+
+            if (read(STDIN_FILENO, &c, sizeof(char)) == -1) {
+                std::cerr << "Could not read from stdin\n";
                 return 4;
             }
         }
+
+        if (number != "") {
+            if (write(fd[1], &number, sizeof(number)) == -1) {
+                std::cerr << "Could not write into pipe\n";
+                return 5;
+            }
+        }
+
         close(fd[1]);
     }
 
